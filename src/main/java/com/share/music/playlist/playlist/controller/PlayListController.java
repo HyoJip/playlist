4455efe2817675d4a54e452a479c9f98f0d1b316
kimbaekjoon <majaua@naver.com>
2022-07-07 01:46:13 +0900

    파일 생성

---
+++ src/main/java/com/share/music/playlist/playlist/controller/PlayListController.java
@@ -0,0 +1,12 @@
+package com.share.music.playlist.playlist.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/playlist")
+@RequiredArgsConstructor
+public class PlayListController {
+
+}
